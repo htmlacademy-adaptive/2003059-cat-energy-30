0565ab2a5713b60b88b4b5da060232f2f534ffd1
Alexandra Ostapenko <fantommyfa@mail.ru>
2024-01-06 19:25:47 +0500

    Декоративные элементы всех страниц

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,88 @@
-/* в этот файл добавляет скрипты*/
+// const navMain = document.querySelector('.main-nav');
+// const navToggle = document.querySelector('.main-nav__toggle');
+
+// navMain.classList.remove('main-nav--nojs');
+
+// function toggleMenu() {
+//   if (navMain.classList.contains('main-nav--close')) {
+//     navMain.classList.remove('main-nav--close');
+//     navMain.classList.add('main-nav--open');
+//   } else {
+//     navMain.classList.add('main-nav--close');
+//     navMain.classList.remove('main-nav--open');
+//   }
+// }
+
+// navToggle.addEventListener('click', toggleMenu);
+
+// const slider = document.querySelector('.comparison__images');
+// const before = document.querySelector('.comparison__image-wrapper--before');
+// const change = document.querySelector('.comparison__range');
+// const body = document.body;
+
+// let isActive = false;
+
+// change.addEventListener('mousedown', () => {
+//   isActive = true;
+// });
+
+// body.addEventListener('mouseup', () => {
+//   isActive = false;
+// });
+
+// body.addEventListener('mouseleave', () => {
+//   isActive = false;
+// });
+
+// const beforeAfterSlider = (x) => {
+//   const shift = Math.max(0, Math.min(x, slider.offsetWidth));
+//   before.style.width = `${shift}px`;
+//   change.style.left = `${shift}px`;
+// };
+
+// const pauseEvents = (e) => {
+//   e.stopPropagation();
+//   e.preventDefault();
+//   return false;
+// };
+
+// body.addEventListener('mousemove', (e) => {
+//   if (!isActive) {
+//     return;
+//   }
+
+//   let x = e.pageX;
+//   x -= slider.getBoundingClientRect().left;
+//   beforeAfterSlider(x);
+//   pauseEvents(e);
+// });
+
+// change.addEventListener('touchstart', () => {
+//   isActive = true;
+// });
+
+// body.addEventListener('touchend', () => {
+//   isActive = false;
+// });
+
+// body.addEventListener('touchcancel', () => {
+//   isActive = false;
+// });
+
+// body.addEventListener('touchmove', (e) => {
+//   if (!isActive) {
+//     return;
+//   }
+
+//   let x;
+
+//   let i;
+//   for (i = 0; i < e.changedTouches.length; i++) {
+//     x = e.changedTouches[i].pageX;
+//   }
+
+//   x -= slider.getBoundingClientRect().left;
+
+//   beforeAfterSlider(x);
+//   pauseEvents(e);
+// });
