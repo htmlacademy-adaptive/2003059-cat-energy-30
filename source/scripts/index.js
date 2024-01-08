39d5742f4df8699e0e69faac904bbe3d12756827
Alexandra Ostapenko <fantommyfa@mail.ru>
2024-01-08 21:08:09 +0500

    Комментарии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -27,41 +27,48 @@ const body = document.body;
 
 let isActive = false;
 
-change.addEventListener('mousedown', () => {
-  isActive = true;
-});
-
+// при нажатии мышкой и удержании
 body.addEventListener('mouseup', () => {
   isActive = false;
 });
 
+// при отпускании мышки
+body.addEventListener('mousedown', () => {
+  isActive = true;
+});
+
+// мышка ушла за пределы
 body.addEventListener('mouseleave', () => {
   isActive = false;
 });
 
+// x - позиция по оси x, находим значение от 0 до максимума - не выходя за пределы
 const beforeAfterSlider = (x) => {
   const shift = Math.max(0, Math.min(x, slider.offsetWidth));
   before.style.width = `${shift}px`;
   change.style.left = `${shift}px`;
 };
 
+// функция останавливает событие после скролла
 const pauseEvents = (e) => {
   e.stopPropagation();
   e.preventDefault();
   return false;
 };
 
+//двигаем мышкой
 body.addEventListener('mousemove', (e) => {
   if (!isActive) {
-    return;
+    return; //если mousemove false - выходим из функции
   }
 
-  let x = e.pageX;
+  let x = e.pageX; // узнаем значение координаты мышки при вводе
   x -= slider.getBoundingClientRect().left;
   beforeAfterSlider(x);
   pauseEvents(e);
 });
 
+// для мобильных устройств
 change.addEventListener('touchstart', () => {
   isActive = true;
 });
@@ -80,8 +87,9 @@ body.addEventListener('touchmove', (e) => {
   }
 
   let x;
-
   let i;
+
+  // changedTouches - список точек касания (по которым прошел палец во время touch)
   for (i = 0; i < e.changedTouches.length; i++) {
     x = e.changedTouches[i].pageX;
   }
